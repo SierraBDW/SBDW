236a561a729e8cba33b407479bcd5a8b6481e40e
Kim Duclos <kimduclos@gmail.com>
2019-09-18 18:11:27 -0600

    calendar in place

--- src/calendar/calendar.jsx
+++ src/calendar/calendar.jsx
@@ -1,17 +1,32 @@
-import BigCalendar from 'react-big-calendar'
-import moment from 'moment'
+import React from "react";
+import Iframe from "react-iframe";
+import Script from "react-inline-script";
+import "./calendar.css";
 
-// Setup the localizer by providing the moment (or globalize) Object
-// to the correct localizer.
-const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
 
-const MyCalendar = props => (
-  <div>
-    <BigCalendar
-      localizer={localizer}
-      events={myEventsList}
-      startAccessor="start"
-      endAccessor="end"
-    />
-  </div>
-)
+const Calendar = () => {
+  return (
+    <div>
+      <Iframe className="iFrame"
+        src="https://kimduclos.youcanbook.me/?noframe=true&skipHeaderFooter=true" 
+        id="ycbmiframekimduclos" 
+        style=
+          "width:100%;
+          height:100%;
+          border:0px;
+          background-color:transparent;" 
+        frameborder="0" 
+        allowtransparency="true">
+      </Iframe>
+      <Script>
+        {`window.addEventListener && window.addEventListener("message", function(event){
+        if (event.origin === "https://kimduclos.youcanbook.me"){
+          document.getElementById("ycbmiframekimduclos").style.height = event.data + "px";
+          }
+        }, false);`}
+      </Script>
+    </div>
+  );
+}
+
+export default Calendar;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "../navBar/navBar";
 import Home from "../home/home";
+import Calendar from "../calendar/calendar";
 
 class Routes extends Component {
     constructor() {
@@ -15,6 +16,7 @@ class Routes extends Component {
                 <Router>
                     <NavBar />
                     <Route exact path= "/" component={ Home } />
+                    <Route path="/calendar" component={ Calendar } />
                 </Router>
             </div>
         )
