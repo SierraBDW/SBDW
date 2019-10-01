2d0b0ba58fc52d2c554682358e9b37ebd55d474b
Kim Duclos <kimduclos@gmail.com>
2019-10-01 07:07:50 -0600

    some styling changes

--- src/home/learnMore/learnMore.jsx
+++ src/home/learnMore/learnMore.jsx
@@ -43,25 +43,6 @@ const LearnMore = () => {
           </p>
         </div>
       </ContentBox>
-      <ContentBox id="cb3">
-        <div class="contentText">
-          <h1 className="title">Title Three</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <img
-            className="contentImg"
-            src={require("../learnMore/images/dogs3.jpg")}
-            alt="dog3"
-          />
-        </div>
-      </ContentBox>
     </div>
   );
 };
