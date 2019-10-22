15c972a77ef6de4e8510bfc7fbdcf8cf09c38626
Kim Duclos <kimduclos@gmail.com>
2019-10-21 20:17:10 -0600

    admin.jsx updated to connect to backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 const express = require("express"); // import the express server
-
+const cors = require("cors");
 const server = express(); // creates the server
 
-// handle requests to the root of the api, the / route (endpoint))
-server.get('/', (req, res) => {
-    res.send("Hello from Express");
-});
+
 
 // watch for connections on port 5000
 server.listen(5000, () =>
     console.log("Server running on http://localhost:5000")
 );
 
-// small change for deploy
+server.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// handle requests to the root of the api, the / route (endpoint))
+server.get('/', (req, res) => {
+    res.send("Hello from Express");
+});
+
+server.post("/clientValues", (req, res) => {
+    res.send("ok")
+});
--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -1,87 +1,187 @@
-import React from "react";
+import React, { useState } from "react";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import Input from "@material-ui/core/Input";
 import "./admin.scss";
-// import axios from "axios";
+import axios from "axios";
 // import FormHelperText from "@material-ui/core/FormHelperText";
 
 const Admin = () => {
+  const [clientValues, setClientValues] = useState({
+    name: "",
+    phone: "",
+    petName: "",
+    eContact: "",
+    email: ""
+  });
+  const [employeeValues, setEmployeeValues] = useState({
+    name: "",
+    phone: "",
+    petName: "",
+    eContact: "",
+    email: ""
+  });
+
+  const handleClientChange = key => e => {
+    setClientValues({ ...clientValues, [key]: e.target.value });
+  };
+  const handleEmployeeChange = key => e => {
+    setEmployeeValues({ ...employeeValues, [key]: e.target.value });
+  };
+
   return (
     <div className="forms">
-      <div className="new-client-form">
+      <form
+        onSubmit={event => {
+          console.log(clientValues);
+          event.preventDefault();
+          axios
+            .post("http://localhost:5000/clientValues", clientValues)
+            .then(response => {
+              console.log(response);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }}
+        className="new-client-form"
+      >
         <div>
           <div className="new-client=title">
             <h2>New Client Entry</h2>
           </div>
           <FormControl>
             <InputLabel htmlFor="my-input">Name</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={clientValues.name}
+              onChange={handleClientChange("name")}
+            />
           </FormControl>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">Phone</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={clientValues.phone}
+              onChange={handleClientChange("phone")}
+            />
           </FormControl>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">Pet Name</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={clientValues.petName}
+              onChange={handleClientChange("petName")}
+            />
           </FormControl>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">Emergency Contact</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={clientValues.eContact}
+              onChange={handleClientChange("eContact")}
+            />
           </FormControl>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">Email address</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={clientValues.email}
+              onChange={handleClientChange("email")}
+            />
           </FormControl>
         </div>
-        <button>Submit</button>
-      </div>
+        <button type="submit">Submit</button>
+      </form>
 
-      <div className="new-employee-form">
+      <form
+        onSubmit={event => {
+          console.log(employeeValues);
+          event.preventDefault();
+           axios
+            .post("/employeeValues", clientValues)
+            .then(response => {
+              console.log(response);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }}
+        className="new-employee-form"
+      >
         <div className="New Employee Entry">
           <h2>New Employee Entry</h2>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">Name</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={employeeValues.name}
+              onChange={handleEmployeeChange("name")}
+            />
           </FormControl>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">Phone</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={employeeValues.phone}
+              onChange={handleEmployeeChange("phone")}
+            />
           </FormControl>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">Pet Name</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={employeeValues.petName}
+              onChange={handleEmployeeChange("petName")}
+            />
           </FormControl>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">Emergency Contact</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={employeeValues.eContact}
+              onChange={handleEmployeeChange("eContact")}
+            />
           </FormControl>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">Email address</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input
+              id="my-input"
+              aria-describedby="my-helper-text"
+              value={employeeValues.email}
+              onChange={handleEmployeeChange("email")}
+            />
           </FormControl>
         </div>
-        <button>Submit</button>
-      </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
